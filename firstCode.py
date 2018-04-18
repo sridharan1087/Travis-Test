458ae1b441ec3b3002f757e0e4fa1b3b557f08c3
srivasan s <srivsrid@in.ibm.com>
2018-04-19 00:07:40 +0530

    first commit

---
+++ firstCode.py
@@ -0,0 +1,11 @@
+import nltk
+import logging
+
+logging.basicConfig(format='(asctime)s (message)s (name)s',level=logging.DEBUG)
+logger = logging.getLogger('firstCode')
+
+def name():
+    logging.info('running the firstCode')
+
+
+    
